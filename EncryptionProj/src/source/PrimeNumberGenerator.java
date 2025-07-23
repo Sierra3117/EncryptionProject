05fafba17c0210eb00b7c9068b928e117ddd8a26
Sierra3117 <ibrahimmrafiq@hotmail.com>
2025-07-23 11:31:05 +0100

    Move Prime generator into own class

---
+++ EncryptionProj/src/source/PrimeNumberGenerator.java
@@ -0,0 +1,37 @@
+package source;
+import java.util.Random;
+
+public class PrimeNumberGenerator {
+	
+	
+    boolean isPrime(int n)
+    {
+        // Corner case
+        if (n <= 1)
+            return false;
+
+        // Check from 2 to sqrt(n)
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+    
+    public  void CreatePri() {
+		Random rand = new Random();
+    	int n;
+		do {
+		n = rand.nextInt(50);
+		System.out.println(n);
+		int num = n;
+        if (isPrime(num)) {
+            System.out.println(num + " is prime");
+        }
+        else {
+            System.out.println(num + " is not prime");
+        }
+		}while (isPrime(n)!= true);
+
+    }
+}
